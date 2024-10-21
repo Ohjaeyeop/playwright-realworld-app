06bf34ffe38b60176629ac4c0cc6686304f3b3c2
jaeyeop.oh <jaeyeop.oh@navercorp.com>
2024-10-21 20:21:26 +0900

    테스트 작성

---
+++ playwright/constants.ts
@@ -0,0 +1,2 @@
+export const API_URL = "http://localhost:3001";
+export const TEST_DATA_API_ENDPOINT = `${API_URL}/testData`;
--- playwright/tests/auth.spec.ts
+++ playwright/tests/auth.spec.ts
@@ -1,3 +1,37 @@
-import { test, expect } from "@playwright/test";
+import axios from "axios";
 
-test.describe("User Sign-up and Login", () => {});
+import { test, expect } from "@playwright/test";
+import { API_URL, TEST_DATA_API_ENDPOINT } from "../constants";
+
+const apiGraphQL = `${API_URL}/graphql`;
+
+test.describe("User Sign-up and Login", () => {
+  test.beforeEach(async ({ page }) => {
+    await axios.post(`${TEST_DATA_API_ENDPOINT}/seed`);
+
+    // await page.route(apiGraphQL, (route, req) => {
+    //   if (route.request().method() === "POST") {
+    //     const { postData } = route.request();
+    //     const data = postData();
+
+    //     if (data !== null) {
+    //       const body = JSON.parse(data);
+    //       if (body.hasOwnProperty("operationName") && body.operationName === "CreateBankAccount") {
+    //         req.alias = "gqlCreateBankAccountMutation";
+    //       }
+    //     }
+    //   }
+
+    //   route.continue();
+    // });
+  });
+
+  test("should redirect unauthenticated user to signin page", async ({ page }) => {
+    await page.goto("/personal");
+
+    const currentPath = new URL(page.url()).pathname;
+    expect(currentPath).toBe("/signin");
+
+    await page.screenshot({ path: "screenshot-signin.png" });
+  });
+});
