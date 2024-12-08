0d7b938a9e34142856d5ebcb940912b16514903a
jaeyeop.oh <jaeyeop.oh@navercorp.com>
2024-12-08 18:19:34 +0900

    회원가입/로그인 테스트

--- playwright/tests/auth.spec.ts
+++ playwright/tests/auth.spec.ts
@@ -1,29 +1,63 @@
-import axios from "axios";
-
 import { test, expect } from "@playwright/test";
 import { API_URL, TEST_DATA_API_ENDPOINT } from "../constants";
 
 const apiGraphQL = `${API_URL}/graphql`;
 
+test.describe("UnauthenticatedUser Login", () => {
+  test("should redirect unauthenticated user to signin page", async ({ page }) => {
+    await page.goto("/personal");
+
+    const currentPath = new URL(page.url()).pathname;
+    expect(currentPath).toBe("/signin");
+
+    await page.screenshot({ path: "screenshot-signin.png" });
+  });
+});
+
 test.describe("User Sign-up and Login", () => {
-  test.beforeEach(async ({ page }) => {
-    await axios.post(`${TEST_DATA_API_ENDPOINT}/seed`);
+  test("signup", async ({ page }) => {
+    await page.goto("/signup");
 
-    // await page.route(apiGraphQL, (route, req) => {
-    //   if (route.request().method() === "POST") {
-    //     const { postData } = route.request();
-    //     const data = postData();
+    const signUpButton = page.locator('button[type="submit"]');
+    await page.click('button[type="submit"]');
+    await expect(signUpButton).toBeDisabled();
+    await expect(page.locator("text=First Name is required")).toBeVisible();
 
-    //     if (data !== null) {
-    //       const body = JSON.parse(data);
-    //       if (body.hasOwnProperty("operationName") && body.operationName === "CreateBankAccount") {
-    //         req.alias = "gqlCreateBankAccountMutation";
-    //       }
-    //     }
-    //   }
+    await page.fill('input[name="firstName"]', "O");
+    await page.fill('input[name="lastName"]', "J");
+    await page.fill('input[name="username"]', "test1234");
+    await page.fill('input[name="password"]', "t");
+    await expect(page.locator("text=Password must contain at least 4 characters")).toBeVisible();
 
-    //   route.continue();
-    // });
+    await page.fill('input[name="password"]', "test1234");
+    await page.fill('input[name="confirmPassword"]', "test123");
+    await expect(page.locator("text=Password does not match")).toBeVisible();
+
+    await page.fill('input[name="confirmPassword"]', "test1234");
+    await page.click('button[type="submit"]');
+
+    await expect(page).toHaveURL("/signin");
+  });
+});
+
+test.describe("User Sign-up and Login", () => {
+  test.beforeEach(async ({ request, page }) => {
+    // await request.post(`${TEST_DATA_API_ENDPOINT}/seed`);
+
+    await page.goto("/signup");
+    await page.fill('input[name="firstName"]', "O");
+    await page.fill('input[name="lastName"]', "J");
+    await page.fill('input[name="username"]', "test1234");
+    await page.fill('input[name="password"]', "t");
+    await expect(page.locator("text=Password must contain at least 4 characters")).toBeVisible();
+
+    await page.fill('input[name="password"]', "test1234");
+    await page.fill('input[name="confirmPassword"]', "test123");
+    await expect(page.locator("text=Password does not match")).toBeVisible();
+
+    await page.fill('input[name="confirmPassword"]', "test1234");
+    await page.click('button[type="submit"]');
+    await expect(page).toHaveURL("/signin");
   });
 
   test("should redirect unauthenticated user to signin page", async ({ page }) => {
@@ -33,5 +67,16 @@ test.describe("User Sign-up and Login", () => {
     expect(currentPath).toBe("/signin");
 
     await page.screenshot({ path: "screenshot-signin.png" });
+  });
+
+  test("should redirect to the home page after login", async ({ page }) => {
+    await page.goto("/signin");
+
+    await page.fill("input[id='username']", "test1234");
+    await page.fill("input[id='password']", "test1234");
+
+    await page.click('button[type="submit"]');
+
+    await expect(page).toHaveURL("/");
   });
 });
